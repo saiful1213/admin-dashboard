7393343f58ab53641edecabed83e728b807b3aec
saiful islam <saifulislamptcbd@gmail.com>
2025-02-05 23:22:39 +0600

    basic layout structure done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,38 @@
+import { Input } from "../ui/input";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { ModeToggle } from "../mode-toggle";
+
 
 const Navbar = () => {
     return (
-        <div>
+        <div className="border-b w-full h-16 flex items-center gap-10 px-5 py-2 rounded-b">
             <h1 className='text-2xl font-bold'>Navbar </h1>
+            <Input type="search" placeholder="Search" />
+            <ModeToggle />
+            <DropdownMenu>
+                <DropdownMenuTrigger>
+                    <Avatar>
+                        <AvatarImage src="https://github.com/shadcn.png" />
+                        <AvatarFallback>CN</AvatarFallback>
+                    </Avatar>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                    <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem>Profile</DropdownMenuItem>
+                    <DropdownMenuItem>Billing</DropdownMenuItem>
+                    <DropdownMenuItem>Team</DropdownMenuItem>
+                    <DropdownMenuItem>Subscription</DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
         </div>
     )
 }
--- src/layout/DashboardLayout.jsx
+++ src/layout/DashboardLayout.jsx
@@ -1,14 +1,18 @@
 import { AppSidebar } from "@/components/app-sidebar";
+import Navbar from "@/components/Navbar/Navbar";
+import { ThemeProvider } from "@/components/theme-provider";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 
 const DashboardLayout = () => {
     return (
         <div className="font-inter max-w-7xl mx-auto">
-            <SidebarProvider>
-                <AppSidebar />
-                <SidebarTrigger />
-                <h1 className="text-5xl">Test</h1>
-            </SidebarProvider>
+            <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+                <SidebarProvider>
+                    <AppSidebar />
+                    <SidebarTrigger />
+                    <Navbar />
+                </SidebarProvider>
+            </ThemeProvider>
         </div>
     )
 }
