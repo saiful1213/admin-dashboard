b1541f26b0efabde3ac651542d2deb98a11f9dd3
saiful islam <saifulislamptcbd@gmail.com>
2025-02-06 00:37:08 +0600

    navigation menu completed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,30 +9,41 @@ import {
 } from "@/components/ui/dropdown-menu"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { ModeToggle } from "../mode-toggle";
-
+import { IoSearchSharp } from "react-icons/io5";
+import { Button } from "../ui/button";
+import { IoIosArrowDown } from "react-icons/io";
 
 const Navbar = () => {
     return (
-        <div className="border-b w-full h-16 flex items-center gap-10 px-5 py-2 rounded-b">
-            <h1 className='text-2xl font-bold'>Navbar </h1>
-            <Input type="search" placeholder="Search" />
-            <ModeToggle />
-            <DropdownMenu>
-                <DropdownMenuTrigger>
-                    <Avatar>
-                        <AvatarImage src="https://github.com/shadcn.png" />
-                        <AvatarFallback>CN</AvatarFallback>
-                    </Avatar>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent>
-                    <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuItem>Profile</DropdownMenuItem>
-                    <DropdownMenuItem>Billing</DropdownMenuItem>
-                    <DropdownMenuItem>Team</DropdownMenuItem>
-                    <DropdownMenuItem>Subscription</DropdownMenuItem>
-                </DropdownMenuContent>
-            </DropdownMenu>
+        <div className="border-b w-full h-16 flex items-center gap-10 px-5 py-2 rounded-b justify-between">
+            <div className="flex gap-5">
+                <div className="flex items-center relative">
+                    <IoSearchSharp className="absolute left-3" />
+                    <Input type="search" placeholder="Search here..." className="pl-9" />
+                </div>
+                <div>
+                    <Button variant="outline">Sort By <IoIosArrowDown /></Button>
+                </div>
+            </div>
+            <div className="flex items-center gap-5">
+                <ModeToggle />
+                <DropdownMenu>
+                    <DropdownMenuTrigger>
+                        <Avatar>
+                            <AvatarImage src="https://github.com/shadcn.png" />
+                            <AvatarFallback>CN</AvatarFallback>
+                        </Avatar>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent>
+                        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem>Profile</DropdownMenuItem>
+                        <DropdownMenuItem>Billing</DropdownMenuItem>
+                        <DropdownMenuItem>Team</DropdownMenuItem>
+                        <DropdownMenuItem>Subscription</DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            </div>
         </div>
     )
 }
--- src/components/app-sidebar.jsx
+++ src/components/app-sidebar.jsx
@@ -1,62 +1,50 @@
-
-import { Calendar, Home, Inbox, Search, Settings } from "lucide-react"
-
+import { HiUsers } from "react-icons/hi";
+import { AiOutlineProduct } from "react-icons/ai";
 import {
     Sidebar,
     SidebarContent,
     SidebarGroup,
     SidebarGroupContent,
     SidebarGroupLabel,
+    SidebarHeader,
     SidebarMenu,
     SidebarMenuButton,
     SidebarMenuItem,
 } from "@/components/ui/sidebar"
+import { Link } from "react-router";
 
 
-// Menu items.
 const items = [
     {
-        title: "Home",
-        url: "#",
-        icon: Home,
+        title: "All Users",
+        url: "/users",
+        icon: <HiUsers />,
     },
     {
-        title: "Inbox",
-        url: "#",
-        icon: Inbox,
-    },
-    {
-        title: "Calendar",
-        url: "#",
-        icon: Calendar,
-    },
-    {
-        title: "Search",
-        url: "#",
-        icon: Search,
-    },
-    {
-        title: "Settings",
-        url: "#",
-        icon: Settings,
+        title: "Products",
+        url: "/products",
+        icon: <AiOutlineProduct />,
     },
 ]
 
 export function AppSidebar() {
     return (
         <Sidebar>
+            <SidebarHeader className="text-2xl font-semibold" >
+                Admin Dashboard
+            </SidebarHeader>
             <SidebarContent>
                 <SidebarGroup>
-                    <SidebarGroupLabel>Application</SidebarGroupLabel>
+                    <SidebarGroupLabel className="uppercase">Overview</SidebarGroupLabel>
                     <SidebarGroupContent>
                         <SidebarMenu>
                             {items.map((item) => (
                                 <SidebarMenuItem key={item.title}>
                                     <SidebarMenuButton asChild>
-                                        <a href={item.url}>
-                                            <item.icon />
+                                        <Link to={item.url}>
+                                            {item.icon}
                                             <span>{item.title}</span>
-                                        </a>
+                                        </Link>
                                     </SidebarMenuButton>
                                 </SidebarMenuItem>
                             ))}
